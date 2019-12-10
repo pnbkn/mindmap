9f5a61433825c00270163665020bf7870ddc06f5
shruti gudi <shrutigudi@gmail.com>
2019-12-10 07:20:02 -0800

    login and register componentsand store added

--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Login } from './Login';
+import { Register } from './Register';
 
 
 export const Home = () => {
   return (
     <div>
+      <Login />
     </div>
   )
 }
