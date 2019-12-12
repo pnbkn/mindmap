c0018806fb40b02e0dfd5be69dbd1f6b442fd3ff
pnbkn <pnblackburn@gmail.com>
2019-12-11 21:34:22 -0800

    Updated syncandseed and db files

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,32 @@
 const Sequelize = require("sequelize");
 const conn = require("./conn");
+// const syncAndSeed = require("./syncandseed");
+//Models
+const User = require("./models/User");
+const Subject = require("./models/Subject");
 const Node = require("./models/Node");
 
 const syncAndSeed = require("./syncandseed");
+
+//Associations
+Subject.belongsTo(User);
+User.hasMany(Subject);
+
+Node.belongsTo(User);
+User.hasMany(Node);
+
+Node.belongsTo(Subject);
+Subject.hasMany(Node);
+
+Node.belongsTo(Node, { as: "parent" });
+Node.hasMany(Node, { as: "child" });
 
 module.exports = {
   syncAndSeed,
   conn,
   models: {
+    User,
+    Subject,
     Node
   }
 };
